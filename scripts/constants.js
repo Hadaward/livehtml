01ad4f011365453ab5a3ae64dcd51eabc2ab043e
Eduardo Gimenez <24706265+Hadaward@users.noreply.github.com>
2023-05-20 19:04:54 -0300

    Delete scripts directory

--- scripts/constants.js
+++
@@ -1,2 +0,0 @@
-export const FPS = 60;
-export const FPS_INTERVAL = Math.floor(1000 / FPS);
--- scripts/math/utils.js
+++
@@ -1,3 +0,0 @@
-export function circleRadiusFromSize(width, height) {
-    return ((width * width) / (8 * height) + height / 2);
-}
